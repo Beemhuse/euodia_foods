4247c48890ce3563eaffe86c0fc8d6fab264f39d
Beemhuse <Brightawah94@gmail.com>
2024-08-22 23:48:40 -0700

    fix: all corrections

--- app/api/password-change/route.js
+++ app/api/password-change/route.js
@@ -23,7 +23,7 @@ export async function POST(req) {
     await client
       .patch(user._id)
       .set({ password: hashedPassword,
-        anonymousUser: false, // Switch anonymousUser to false
+        isAnonymous: false, // Switch anonymousUser to false
 
        })
       .unset(['resetToken', 'resetTokenExpiry'])
