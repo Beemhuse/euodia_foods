41e14dd1fca3fff96ca8f9cf866cae151c203e9e
Teslim <yusuftessy2005@gmail.com>
2024-08-24 17:39:00 +0100

    Update .env file, Hero component, and Sanity client configuration

--- utils/sanity/client.js
+++ utils/sanity/client.js
@@ -4,7 +4,6 @@ import { v4 as uuidv4 } from 'uuid'; // Import the UUID library for generating u
 
 export const client = createClient({
   projectId: '8bms2xqg',
-  // dataset: 'main',
   dataset: 'production',
   apiVersion: '2024-03-11',
   useCdn: true,
