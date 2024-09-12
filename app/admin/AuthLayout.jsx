f60d6fe2926f10adae5c0f7e567f121849ad9c88
Beemhuse <Brightawah94@gmail.com>
2024-09-12 11:52:19 -0700

    fix: token and admin

--- app/admin/AuthLayout.jsx
+++ app/admin/AuthLayout.jsx
@@ -1,18 +1,13 @@
 "use client"
 
 
-import { getCookie } from '@/utils/getCookie'
-import { useRouter } from 'next/navigation'
+import checkTokenExpirationMiddleware from '@/middleware/checkTokenMiddleware';
 import React, { Fragment, useEffect } from 'react'
 
 export default function AuthLayout({children}) {
-    const cookie = getCookie("admineu_token")
-const router = useRouter()
-useEffect(()=>{
-if(!cookie){
-return router.push("/")
-}
-}, [router, cookie])
+  useEffect(() => {
+    checkTokenExpirationMiddleware();
+  }, []);
   return (
     <Fragment>{children}</Fragment>
   )
