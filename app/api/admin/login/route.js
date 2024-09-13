91530261924128fb2451ed292d5928200b470552
Beemhuse <Brightawah94@gmail.com>
2024-09-12 21:29:10 -0700

    fix: token and admin

--- app/api/admin/login/route.js
+++ app/api/admin/login/route.js
@@ -58,7 +58,7 @@ export async function POST(req) {
     const token = jwt.sign(
       { userId: admin._id, role: 'admin' },
       secretKey,
-      { expiresIn: '5min' }
+      { expiresIn: '5h' }
     );
 
     return new Response(
